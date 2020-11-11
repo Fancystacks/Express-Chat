90070d24a30818ed506942b753bc4509de7fc276
Fancy Stacks <fancystacks@protonmail.com>
2020-11-11 01:53:14 -0700

    socket events emitted 💬

--- server.js
+++ server.js
@@ -9,10 +9,19 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connect', (socket) => {
+io.on('connection', (socket) => {
     console.log("New Web Socket Connection...");
-})
+
+    socket.emit('message', 'Welcome to Express Chat!');
+
+    socket.broadcast.emit('message', 'A user has joined the chat'); // broadcast to all other users
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+});
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
-
