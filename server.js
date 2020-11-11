4110bca1464c84a2c445908aa5fc5c3181b6eebe
Fancy Stacks <fancystacks@protonmail.com>
2020-11-11 00:45:18 -0700

    set up socket.io  💬

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+io.on('connect', (socket) => {
+    console.log("New Web Socket Connection...");
+})
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
 
