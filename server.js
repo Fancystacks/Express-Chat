b90a1be9aa2a099ad6de5b05f12a643d0076a91b
Fancy Stacks <fancystacks@protonmail.com>
2020-11-10 23:51:07 -0700

    awesome styles 💬

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
 
-const PORT = process.env.PORT || PORT;
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
 
