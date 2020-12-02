da4bacf8d4727244c932cdafe7bb6f60648dfd8b
Fancy Stacks <fancystacks@protonmail.com>
2020-12-02 16:53:18 -0700

    sending message 💬

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', (message) => {
     console.log(message);
-})
+});
+
+chatForm.addEventListener('submit' , (event) => {
+    event.preventDefault();
+    const msg = event.target.elements.msg.value;
+    console.log(msg);
+});
